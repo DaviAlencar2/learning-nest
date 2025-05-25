f5aaabb082efca1f61a04ae349c2f4c77959b86d
Davi Leite Alencar <davilt.alencar@gmail.com>
2025-05-25 09:10:15 -0300

    validando message com class-validator, class-transformer e whitelist.

--- src/message/dto/update-message.dto.ts
+++ src/message/dto/update-message.dto.ts
@@ -1,5 +1,10 @@
+import { IsString } from 'class-validator';
+
 export class UpdateMessageDto {
+  @IsString()
   readonly from?: string;
+  @IsString()
   readonly to?: string;
+  @IsString()
   readonly content?: string;
 }
