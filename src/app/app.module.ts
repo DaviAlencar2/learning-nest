7fbe0e4fc3e646eeb8eecfa724c1f28f076e0def
Davi Leite Alencar <davilt.alencar@gmail.com>
2025-05-26 21:12:36 -0300

    configuracoes inciais para usar o typeorm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessageModule } from 'src/message/message.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MessageModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true, // Depois usar migrations!!!
+    }),
+    MessageModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,10 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
-  }
-
-  getOla(): string {
-    return 'ola';
+    return 'API APENAS DE ESTUDO!';
   }
 }
